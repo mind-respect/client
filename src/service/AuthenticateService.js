cd25115be76ed88809ac04f2c478bbfd236416fa
Vincent Blouin <vincent.blouin@gmail.com>
2019-03-13 14:00:57 -0400

    persistent session

--- src/service/AuthenticateService.js
+++ src/service/AuthenticateService.js
@@ -6,12 +6,14 @@ import Service from '@/Service'
 export default {
     register: function (user) {
         user.user_name = user.username;
+        user.staySignedIn = true;
         return Service.api().post("/users/", user);
     },
     login: function (user) {
+        user.staySignedIn = true;
         return Service.api().post("/users/session", user);
     },
-    logout: function(){
+    logout: function () {
         return Service.api().delete("/users/session");
     }
 };
