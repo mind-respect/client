9b09abef0596d25b4175078fc4f69b7502eb4765
Vincent Blouin <vincent.blouin@gmail.com>
2019-09-11 17:26:34 -0400

    no refresh on page access

--- src/router.js
+++ src/router.js
@@ -5,69 +5,79 @@ Vue.use(Router);
 import Store from '@/store'
 
 const router = new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: () => {
-                return Store.state.user === undefined ? import('./views/About.vue') : import('./views/UserHome.vue');
+        mode: 'history',
+        base: process.env.BASE_URL,
+        routes: [
+            {
+                path: '/',
+                name: 'home',
+                component: () => {
+                    if (Store.state.user === undefined) {
+                        return import('./views/About.vue');
+                    }
+                    router.push({
+                        name: "UserHome",
+                        params: {
+                            username: Store.state.user.username
+                        }
+                    });
+                    return import('./views/UserHome.vue');
+                }
+            },
+            {
+                path: '/welcome',
+                name: 'welcome',
+                component: () => import('./views/About.vue')
+            },
+            {
+                path: '/register',
+                name: 'register',
+                component: () => import('./views/About.vue')
+            },
+            {
+                path: '/login',
+                name: 'login',
+                component: () => import('./views/About.vue')
+            },
+            {
+                path: '/forgot-password',
+                name: 'forgotPassword',
+                component: () => import('./views/About.vue')
+            },
+            {
+                path: '/email/:email/token/:changePasswordToken',
+                name: 'changePassword',
+                component: () => import('./views/About.vue')
+            },
+            {
+                path: '/about',
+                name: 'about',
+                // route level code-splitting
+                // this generates a separate chunk (about.[hash].js) for this route
+                // which is lazy-loaded when the route is visited.
+                component: () => import('./views/About.vue')
+            },
+            {
+                path: '/user/:username',
+                name: 'UserHome',
+                component: () => import('./views/UserHome.vue')
+            },
+            {
+                path: '/user/:username/friends',
+                name: 'FriendsUserHome',
+                component: () => import('./views/UserHome.vue')
+            },
+            {
+                path: '/user/:username/requestUser/:requestUsername/destinationUser/:destinationUsername/confirmToken/:confirmToken',
+                name: "ConfirmFriendshipHome",
+                component: () => import('./views/UserHome.vue')
+            },
+            {
+                path: '/user/:username/graph/:graphElementType/:centerUri',
+                name: "Center",
+                component: () => import('./views/Center.vue')
             }
-        },
-        {
-            path: '/welcome',
-            name: 'welcome',
-            component: () => import('./views/About.vue')
-        },
-        {
-            path: '/register',
-            name: 'register',
-            component: () => import('./views/About.vue')
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () => import('./views/About.vue')
-        },
-        {
-            path: '/forgot-password',
-            name: 'forgotPassword',
-            component: () => import('./views/About.vue')
-        },
-        {
-            path: '/email/:email/token/:changePasswordToken',
-            name: 'changePassword',
-            component: () => import('./views/About.vue')
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('./views/About.vue')
-        },
-        {
-            path: '/user/:username',
-            name: 'UserHome',
-            component: () => import('./views/UserHome.vue')
-        },
-        {
-            path: '/user/:username/friends',
-            name: 'FriendsUserHome',
-            component: () => import('./views/UserHome.vue')
-        },
-        {
-            path: '/user/:username/requestUser/:requestUsername/destinationUser/:destinationUsername/confirmToken/:confirmToken',
-            name: "ConfirmFriendshipHome",
-            component: () => import('./views/UserHome.vue')
-        },
-        {
-            path: '/user/:username/graph/:graphElementType/:centerUri',
-            name: "Center",
-            component: () => import('./views/Center.vue')
-        }
-    ]
-});
+        ]
+    })
+;
 export default router;
