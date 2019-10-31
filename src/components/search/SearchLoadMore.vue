3dc97deb830c3ea2bbc97dfb79dba506be45c17d
Vincent Blouin <vincent.blouin@gmail.com>
2019-10-31 14:51:28 -0400

    load more loading

--- src/components/search/SearchLoadMore.vue
+++ src/components/search/SearchLoadMore.vue
@@ -1,11 +1,11 @@
 <template>
     <v-list-item @click="click" v-show="nbLastResults !== 0 && searchText !== '' && (!noCreateButton || !isAllLoaded)">
-        <v-list-item-content v-show="!isAllLoaded">
+        <v-list-item-content v-show="!isAllLoaded && !isLoading">
             <v-list-item-title class="text-center text-uppercase caption">
                 {{$t('moreResults')}}
             </v-list-item-title>
         </v-list-item-content>
-        <v-list-item-content v-show="isAllLoaded && !noCreateButton">
+        <v-list-item-content v-show="isAllLoaded && !noCreateButton && !isLoading">
             <v-list-item-subtitle class="font-weight-bold">
                 <span>
                     {{$t('create')}}
@@ -13,9 +13,12 @@
                 "{{searchText}}"
             </v-list-item-subtitle>
         </v-list-item-content>
-        <v-list-item-action v-show="isAllLoaded && !noCreateButton">
+        <v-list-item-action v-show="isAllLoaded && !noCreateButton && !isLoading">
             <v-icon>add</v-icon>
         </v-list-item-action>
+        <v-list-item-content v-show="isLoading">
+            <v-progress-circular indeterminate color="third"></v-progress-circular>
+        </v-list-item-content>
     </v-list-item>
 </template>
 
@@ -27,7 +30,8 @@
             return {
                 nbLastResults: 0,
                 isAllLoaded: false,
-                searchText: ""
+                searchText: "",
+                isLoading: false
             }
         },
         methods: {
@@ -37,11 +41,16 @@
                 this.searchText = searchText;
             },
             loadMore: function () {
+                this.isLoading = true;
                 this.$emit('loadMore', (nbLastResults) => {
                     this.nbLastResults = nbLastResults;
+                    this.isLoading = false;
                 });
             },
             click: function () {
+                if (this.isLoading) {
+                    return;
+                }
                 this.isAllLoaded ? this.$emit('create') : this.loadMore();
             }
         },
