8b5d20cf0c9ee027dacdcd367683593f5937a479
Vincent Blouin <vincent.blouin@gmail.com>
2020-04-03 16:17:06 -0400

    prevents from setting left right when under a group relation under a non center vertex

--- src/graph-element/GraphElement.js
+++ src/graph-element/GraphElement.js
@@ -497,19 +497,19 @@ GraphElement.GraphElement.prototype.isTagFromWikipedia = function () {
     return false;
 };
 
-GraphElement.GraphElement.prototype.buildChildrenIndex = function (index, withCenterContextLeftRightIndex) {
-    withCenterContextLeftRightIndex = this.isCenter ? {} : withCenterContextLeftRightIndex || this.getChildrenIndex();
+GraphElement.GraphElement.prototype.buildChildrenIndex = function (index, whenCenterContextLeftRightIndex) {
+    whenCenterContextLeftRightIndex = this.isCenter ? {} : whenCenterContextLeftRightIndex || this.getChildrenIndex();
     index = index || 0;
     return this.getClosestChildrenInTypes(
         [GraphElementType.Vertex, GraphElementType.MetaGroupVertex, GraphElementType.GroupRelation],
         true
     ).reduce((childrenIndex, child) => {
         if (child.isGroupRelation()) {
-            let groupRelationIndex = child.buildChildrenIndex(index, withCenterContextLeftRightIndex);
+            let groupRelationIndex = child.buildChildrenIndex(index, whenCenterContextLeftRightIndex);
             index += Object.keys(groupRelationIndex).length;
             childrenIndex = Object.assign(childrenIndex, groupRelationIndex);
         } else {
-            let leftRightIndexWhileCenter = withCenterContextLeftRightIndex[child.getUri()];
+            let leftRightIndexWhileCenter = whenCenterContextLeftRightIndex[child.getUri()];
             let isLeft = leftRightIndexWhileCenter ? leftRightIndexWhileCenter.toTheLeft : child.isToTheLeft();
             childrenIndex[child.getUri()] = {
                 index: index,
