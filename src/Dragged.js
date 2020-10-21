db0e726e369b8f2e3f45233a5b45f6f0c1783d8e
Vincent Blouin <vincent.blouin@gmail.com>
2020-10-20 21:57:29 -0400

    more accurate move bubbles

--- src/Dragged.js
+++ src/Dragged.js
@@ -35,13 +35,33 @@ const Dragged = {
 
         parent.getDescendants(isLeft, (descendant) => {
             return !Dragged.dragged || descendant.getId() !== Dragged.dragged.getId();
+        }).filter((descendant) => {
+            return descendant.shouldShow();
         }).forEach((child) => {
             child = child.getShownBubble();
-            let position = child.getHtml().getBoundingClientRect();
-            let xPosition = (position.left + position.right) / 2 + document.scrollingElement.scrollLeft;
-            let yPosition = (position.top + position.bottom) / 2 + document.scrollingElement.scrollTop;
-            let xDistance = x - xPosition;
-            let yDistance = y - yPosition;
+            const position = child.getHtml().getBoundingClientRect();
+            const leftPosition = position.left + document.scrollingElement.scrollLeft
+            const rightPosition = position.right + document.scrollingElement.scrollLeft;
+            const topPosition = position.top + document.scrollingElement.scrollTop;
+            const bottomPosition = position.bottom + document.scrollingElement.scrollTop;
+            let closestX;
+            if (x < leftPosition) {
+                closestX = leftPosition;
+            } else if (x > rightPosition) {
+                closestX = rightPosition;
+            } else {
+                closestX = x;
+            }
+            let closestY;
+            if (y < topPosition) {
+                closestY = topPosition;
+            } else if (y > bottomPosition) {
+                closestY = bottomPosition;
+            } else {
+                closestY = y;
+            }
+            let xDistance = x - closestX;
+            let yDistance = y - closestY;
             let distance = Math.hypot(xDistance, yDistance);
             if (Math.abs(distance) < Math.abs(minDistance)) {
                 minDistance = distance;
