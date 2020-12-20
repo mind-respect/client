d38a3db22cedc9d9ccb2464ba5048a99a31828bb
Vincent Blouin <vincent.blouin@gmail.com>
2020-12-20 07:19:51 -0500

    colors

--- src/components/ShareLevelSelection.vue
+++ src/components/ShareLevelSelection.vue
@@ -2,13 +2,13 @@
     <v-radio-group v-model="shareLevelLocal" class="pa-0 mt-0" @change="$emit('update', shareLevelLocal)"
                    :disabled="$store.state.selected.length === 0">
         <v-radio :label="$t('shareSelect:private')" on-icon="lock" off-icon="lock" value="PRIVATE"
-                 color="third"></v-radio>
+                 color="thirdLight"></v-radio>
         <v-radio :label="$t('shareSelect:friendsOnly')" value="FRIENDS" on-icon="people"
-                 off-icon="people" color="third"></v-radio>
+                 off-icon="people" color="thirdLight"></v-radio>
         <v-radio :label="$t('shareSelect:publicWithLink')" value="PUBLIC_WITH_LINK" on-icon="link"
-                 off-icon="link" color="third"></v-radio>
+                 off-icon="link" color="thirdLight"></v-radio>
         <v-radio :label="$t('shareSelect:public')" value="PUBLIC" on-icon="public" off-icon="public"
-                 color="third"></v-radio>
+                 color="thirdLight"></v-radio>
     </v-radio-group>
 </template>
 
